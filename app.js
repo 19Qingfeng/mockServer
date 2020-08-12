8f8a51190bd037d7d47ea3ea6121308159befb16
wanghaoyu1213 <228233464@qq.com>
2020-08-12 09:37:23 +0800

    增加response header

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ const user = require('./routes/user')
 // error handler
 onerror(app)
 
+// 解决跨域
+const handler = async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*"); // 规定允许访问该资源的外域 URI
+  ctx.set("Access-Control-Allow-Methods", "GET"); // 请求方式
+  ctx.set("Access-Control-Max-Age", "3600"); // 设定预检请求结果的缓存时间
+  ctx.set("Access-Control-Allow-Headers", "apk"); //  规定 CORS 请求时会额外发送的头信息字段
+  ctx.set("Access-Control-Allow-Credentials", "true"); // 请求可以带 Cookie 等
+
+  // 针对预检请求
+  if (ctx.request.method == "OPTIONS") {
+    ctx.response.stutas = "200"
+  }
+
+  try {
+    await next();
+    console.log("处理通过");
+  } catch (e) {
+    console.log("处理错误");
+    ctx.response.status = e.statusCode || err.status || 500;
+    ctx.response.body = {
+      message: e.message
+    }
+  }
+}
+// use handler
+app.use(handler)
+
 // middlewares
 app.use(bodyparser({
   enableTypes: ['json', 'form', 'text']
